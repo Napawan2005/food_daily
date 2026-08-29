dc39d9be98429a034bfba4994c0b2921e54e217c
Napassawan Korsinprasert <117275945+Napawan2005@users.noreply.github.com>
2026-08-29 09:25:45 +0700

    [poc_load_date] test : update vaildate test to order_id and rename fiels restaurnt -> restaurant

--- food_daily/tests/assert_stg_food_daily__order_no_full_row_duplicates.sql
+++ food_daily/tests/assert_stg_food_daily__order_no_full_row_duplicates.sql
@@ -1,9 +1,9 @@
 select
     customer_id, order_date, order_time, order_id, items,
-    mode, amount, restaurnt, order_status, ratings, feedback,
+    mode, amount, restaurant, order_status, ratings, feedback,
     count(*) as duplicate_count
 from {{ ref('stg_food_daily__order') }}
 group by
     customer_id, order_date, order_time, order_id, items,
-    mode, amount, restaurnt, order_status, ratings, feedback
+    mode, amount, restaurant, order_status, ratings, feedback
 having duplicate_count > 1
