e1ad67974671a2761212435d007a3383effbeb10
Napassawan Korsinprasert <117275945+Napawan2005@users.noreply.github.com>
2026-09-07 21:50:07 +0700

    [dbt-analytics] feat : add fct_order feat table at order food_daiy

---
+++ food_daily/models/facts/fct_orders.sql
@@ -0,0 +1,38 @@
+{{ config(materialized='table') }}
+
+WITH source AS (
+    SELECT * FROM {{ ref('int_feedback_category') }}
+),
+
+restaurants AS (
+    SELECT * FROM {{ ref('dim_restaurant') }}
+),
+
+statuses AS (
+    SELECT * FROM {{ ref('dim_order_status') }}
+),
+
+payment AS (
+    SELECT * FROM {{ ref('dim_payment_mode') }}
+),
+
+categories AS (
+    SELECT * FROM {{ ref('dim_category') }}
+)
+
+SELECT
+    s.order_id,
+    s.customer_id,
+    r.restaurant_id,
+    su.order_status_id,
+    p.mode_id,
+    c.category_id,
+    s.order_date,
+    s.order_time,
+    s.amount,
+    s.ratings
+FROM source AS s
+LEFT JOIN restaurants AS r  ON s.restaurant   = r.restaurant_name
+LEFT JOIN statuses    AS su ON s.order_status = su.order_status
+LEFT JOIN payment     AS p  ON s.mode         = p.mode
+LEFT JOIN categories  AS c  ON s.category     = c.category
